51742019db12f09254a6cf48045934603fec2d82
andriantsoa <andryrandriadev@gmail.com>
2020-11-14 15:30:21 +0100

    update profil

--- frontend/src/app/dashboard/profil-form/profil-form.component.ts
+++ frontend/src/app/dashboard/profil-form/profil-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Role, Statut } from 'src/app/commun/enum/role.enum';
+import { Secteurs } from 'src/app/commun/constantes/secteur.constant';
 import { ValidationService } from 'src/app/core/components';
 import { ProfilService, UserService } from 'src/app/core/services';
 import { IProfil } from '../../core/interfaces/profil.interface';
@@ -18,9 +19,11 @@ export class ProfilFormComponent implements OnInit {
   public profil: IProfil;
   public roles: any[];
   public status: any[];
+  public secteurs = Secteurs;
 
   constructor(
     private formBuilder: FormBuilder,
+    private router: Router,
     private route: ActivatedRoute, private profilService: ProfilService,
     private validationService: ValidationService,
   ) {
@@ -54,6 +57,27 @@ export class ProfilFormComponent implements OnInit {
           this.step1.controls.status.setValue(this.profil.status);
         });
   }
+
+  private updateProfil(profil: IProfil): void {
+    this.profilService.update(profil)
+      .subscribe(p => {
+        console.log(p);
+        if (profil.role === Role.CANDIDAT) {
+          this.router.navigate(['/dashboard/candidat']);
+        } else if (profil.role === Role.ENTREPRISE) {
+          this.router.navigate(['/dashboard/entreprise']);
+        }
+      });
+  }
+
+  public sendUpdate(): void {
+    const param = {
+      ...this.step1.value,
+      ...this.step2.value
+    } as IProfil;
+    this.updateProfil(param);
+  }
+
 
   // public createExperiences(): void {
   //   this.profilForm = this.formBuilder.group({
